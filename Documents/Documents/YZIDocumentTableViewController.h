b4af2258a1e680d5600d7d0b983d29632265cbd4
yzhukovs <yzhukovs@bumnetworks.com>
2019-01-23 15:51:17 -0500

    storyboard setup

---
+++ Documents/Documents/YZIDocumentTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  YZIDocumentTableViewController.h
+//  Documents
+//
+//  Created by Yvette Zhukovsky on 1/23/19.
+//  Copyright © 2019 Yvette Zhukovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YZIDocumentTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
